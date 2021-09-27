0df34acc624cbfe2926d245d3a50089c3c96d7ed
Fael wild <83206923+fael-wild@users.noreply.github.com>
2021-09-27 18:57:24 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from config import BOT_USERNAME, BOT_NAME, ASSISTANT_NAME, OWNER_NAME, UPDATES_CHANNEL, GROUP_SUPPORT
 from helpers.filters import command
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
-from helpers.decorators import sudo_users_only
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from helpers.decorators import authorized_users_only
 
 
 START_TIME = datetime.utcnow()
@@ -29,10 +29,10 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
+@Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_text(
-        f"""<b> *Halo Cakep. \n
+        f"""<b> **Halo {message.from_user.first_name} Cakep. \n
 Saya Bot Music Group, Yang Bisa Memutar Lagu Dengan Cepat di Voice Chat Group Dengan Cara Yang Mudah
 Saya Memiliki Banyak Fitur Praktis Seperti :
 ┏━━━━━━━━━━━━━━
@@ -40,12 +40,12 @@ Saya Memiliki Banyak Fitur Praktis Seperti :
 ┣• Mendownload Lagu.
 ┣• Melihat Lirik Lagu.
 ┣• Mempunyai Banyak Modul Menarik.
-┣• Sekaligus Bisa Mengatur Groupmu.
+┣• Mencari Lagu Yang ingin di Putar atau di Download.
 ┣• Gunakan Perintah » /help « untuk Mengetahui Fitur Lengkap Saya
 ┗━━━━━━━━━━━━━━
-❃ Project By [Rio Cute](https://t.me/riio00)
+❃ Project By [Fael Wild](https://t.me/FuckTheRulesWild)
 ❃ Terimakasih Telah Menggunakan Project Ini! [{OWNER_NAME}](https://t.me/{OWNER_NAME})
-━━━━━━━━━━━━━━━*
+━━━━━━━━━━━━━━━**
 </b>""",
         reply_markup=InlineKeyboardMarkup(
             [ 
@@ -54,19 +54,19 @@ Saya Memiliki Banyak Fitur Praktis Seperti :
                         "➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
                 ],[
                     InlineKeyboardButton(
-                         "🛠 Repo", url="http://github.com/RioProjectX/Rio-Music"
+                         "❃ Instagram Owner ❃", url="https://instagram.com/fael001_?utm_medium=copy_link"
                     ),
                     InlineKeyboardButton(
                         "🐥 Pemilik Bot", url=f"https://t.me/{OWNER_NAME}")
                 ],[
                     InlineKeyboardButton(
-                        "👥 Official Group", url=f"https://https://t.me/RioGroupSupport"
+                        "👥 Official Group", url=f"https://t.me/wildentourageni"
                     ),
                     InlineKeyboardButton(
-                        "📣 Official Channel", url=f"https://t.me/riobotsupport")
+                        "📣 Official Channel", url=f"https://t.me/letsings")
                 ],[
                     InlineKeyboardButton(
-                        "🤴 Developer", url="https://t.me/riio00"
+                        "🤴 Developer", url="https://t.me/FuckTheRulesWild"
                     )
                 ]
             ]
@@ -81,15 +81,15 @@ async def start(client: Client, message: Message):
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
     await message.reply_text(
-        f"""✅ **Bot Sedang Bekerja**\n<b>💠 **uptime:**</b> `{uptime}`""",
+        f"""✅ **Bot is running Successful**\n\n<b>🎈 **Bot uptime:**</b> `{uptime}`""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "✨ Group", url=f"https://t.me/{GROUP_SUPPORT}"
+                        "Group", url=f"https://t.me/{GROUP_SUPPORT}"
                     ),
                     InlineKeyboardButton(
-                        "📣 Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                        "Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
                     )
                 ]
             ]
@@ -99,29 +99,23 @@ async def start(client: Client, message: Message):
 @Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>👋🏻 **Halo** {message.from_user.mention()}</b>
-
-**Tekan Tombol Di Bawah Ini Untuk Melihat Daftar Perintah !**
-
-⚡ __Powered by {BOT_NAME} A.I""",
+        f"""<b>👋🏻 Hello {message.from_user.mention()}, please tap the button below to see the help message you can read for using this bot</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        text="❔ Cara Menggunakan Bot", callback_data="cbguide"
+                        text="❔ Channel ❔", url="https://t.me/letsings"
                     )
                 ]
             ]
-        ),
+        )
     )
 
 @Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
 async def help_(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>💡 Hello {message.from_user.mention} selamat datang di help menu !</b>
-
-**di menu ini kamu bisa melihat seluruh daftar perintah dari riomusic.**
-
+        f"""<b>💡 Hello {message.from_user.mention} welcome to the help menu !</b>
+**in this menu you can open several available command menus, in each command menu there is also a brief explanation of each command**
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -157,24 +151,25 @@ async def help_(client: Client, message: Message):
 
 
 @Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+@authorized_users_only
 async def ping_pong(client: Client, message: Message):
     start = time()
-    m_reply = await message.reply_text("Mengecek ping...")
+    m_reply = await message.reply_text("_Mengecek Ping_...")
     delta_ping = time() - start
     await m_reply.edit_text(
-        "🏓 `PONG!!`\n"
-        f"⚡️ `{delta_ping * 1000:.3f} ms`"
+        "**Ping Pong!!**\n"
+        f"🔹 `{delta_ping * 1000:.3f} ms`"
     )
 
 
 @Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
-@sudo_users_only
+@authorized_users_only
 async def get_uptime(client: Client, message: Message):
     current_time = datetime.utcnow()
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
     await message.reply_text(
-        "🤖 Status Bot:\n"
-        f"• **uptime:** `{uptime}`\n"
-        f"• **waktu mulai:** `{START_TIME_ISO}`"
+        "🎧 Bot status:\n"
+        f"🔹 **uptime:** `{uptime}`\n"
+        f"🔹 **start time:** `{START_TIME_ISO}`"
     )
